257a269b44db1fd9f1a897a5a5863c954a70bcb7
dak0ta <dak0ta@localhost.localdomain>
2018-03-03 15:40:45 -0800

    working on gd upload functions

--- source/gd_api/up_code.py
+++ source/gd_api/up_code.py
@@ -3,4 +3,20 @@ from pydrive.drive import GoogleDrive
 
 
 def client_auth():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+    drive = GoogleDrive(gauth)
+    return drive
+
+def create_db():
+    drive = client_auth()
+    data = drive.CreateFile({'title': 'task_db.json'})
+    data.Upload()
     
+
+
+def main():
+    create_db()
+
+if __name__ == '__main__':
+    main()
