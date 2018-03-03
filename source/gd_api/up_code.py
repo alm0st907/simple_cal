d052dd182071c711488c339f9ecbb2b30e8664ee
dak0ta <dak0ta@localhost.localdomain>
2018-03-03 15:47:14 -0800

    client_auth finished, working on other functions

--- source/gd_api/up_code.py
+++ source/gd_api/up_code.py
@@ -4,9 +4,23 @@ from pydrive.drive import GoogleDrive
 
 def client_auth():
     gauth = GoogleAuth()
-    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+    gauth.LoadCredentialsFile("user.txt") #try to load credentials for gdrive
+
+    if gauth.credentials is None:
+        #we need to get them if they arent able to be loaded
+        gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+        #refresh the creds if necessary
+    else:
+        gauth.Authorize()
+        #using saved credentials and moving on
+    gauth.SaveCredentialsFile("user.txt")
+    #this saves our credentials so we dont pop the webpage every opening
+
     drive = GoogleDrive(gauth)
-    return drive
+
+    return drive #return usable list file
 
 def create_db():
     drive = client_auth()
