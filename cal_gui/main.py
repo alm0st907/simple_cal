7e2c48127311937a8e4ceb5ee6a11a4b3fa3e092
b900cool <b900cool@gmail.com>
2018-03-03 17:18:05 -0800

    integrated GUI
    integrated list view with basic gui

--- cal_gui/main.py
+++ cal_gui/main.py
@@ -3,6 +3,17 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.listview import ListItemButton
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+
+class ToDoItem(ListItemButton):
+    pass
+
+class ListViewLayout(BoxLayout):
+    pass
 
 class BaseGui(GridLayout):
     pass
